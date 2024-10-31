0891ab536294ca8cda3daf012d971a50840157f6
Aya <ayaelrhayour123@gmail.com>
2024-10-31 14:34:43 +0100

    created surveyEdition entity

--- src/main/java/com/youcode/survey/models/entities/SurveyEdition.java
+++ src/main/java/com/youcode/survey/models/entities/SurveyEdition.java
@@ -1,4 +1,35 @@
 package com.youcode.survey.models.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SurveyEdition {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column
+    private LocalDateTime creationDate;
+
+    @Column
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate Year;
+
 }
