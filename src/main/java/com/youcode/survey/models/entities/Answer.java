c2a9a867c06c2d629402700a9bfc858505414a42
Aya <ayaelrhayour123@gmail.com>
2024-10-31 14:48:17 +0100

    created answer entity

--- src/main/java/com/youcode/survey/models/entities/Answer.java
+++ src/main/java/com/youcode/survey/models/entities/Answer.java
@@ -1,4 +1,32 @@
 package com.youcode.survey.models.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Answer {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column
+    private String text;
+
+    @Column
+    private int selectionCount;
+
+    @Column
+    private int percentage;
 }
