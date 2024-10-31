018bc4289e524d601a5520a938bf9bdf04f1df1b
Aya <ayaelrhayour123@gmail.com>
2024-10-31 14:44:49 +0100

    created question entity

--- src/main/java/com/youcode/survey/models/entities/Question.java
+++ src/main/java/com/youcode/survey/models/entities/Question.java
@@ -1,4 +1,33 @@
 package com.youcode.survey.models.entities;
 
+
+import com.youcode.survey.models.enums.Type;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Question {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column
+    private String text;
+
+    @Column
+    private Type type;
+
+    @Column
+    private int answerCount;
 }
---
+++ src/main/java/com/youcode/survey/models/enums/Type.java
@@ -0,0 +1,6 @@
+package com.youcode.survey.models.enums;
+
+public enum Type {
+    SINGLE_CHOICE,
+    MULTIPLE_CHOICE
+}
