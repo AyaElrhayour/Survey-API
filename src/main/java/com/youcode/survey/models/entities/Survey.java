004d008a2acad512fee1a6adbf63b181f6abc704
Aya <ayaelrhayour123@gmail.com>
2024-10-31 14:30:32 +0100

    created survey entity

--- src/main/java/com/youcode/survey/models/entities/Survey.java
+++ src/main/java/com/youcode/survey/models/entities/Survey.java
@@ -1,4 +1,31 @@
 package com.youcode.survey.models.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Survey {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(unique=true)
+    @NotBlank(message = "The title can't be blank")
+    private String title;
+
+    @Column
+    private String description;
 }
