2a704400c39d3c2361797f1d1f824da27f2f98fe
Aya <ayaelrhayour123@gmail.com>
2024-11-01 10:17:19 +0100

    added reading SurveyEdition  DTO

---
+++ src/main/java/com/youcode/survey/models/dto/SurveyEdition/SurveyEditionReadingDTO.java
@@ -0,0 +1,23 @@
+package com.youcode.survey.models.dto.SurveyEdition;
+
+
+import com.youcode.survey.models.dto.Survey.SurveyReadingEmbdDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SurveyEditionReadingDTO {
+
+    private UUID id;
+    private LocalDateTime creationDate;
+    private LocalDate startDate;
+    private Integer year;
+    private SurveyReadingEmbdDTO survey;
+}
